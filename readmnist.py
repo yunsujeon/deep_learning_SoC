f38277f4c70e1043df4c49f2c3ad0c639a66da30
yunsujeon <dbstndi6316@naver.com>
2020-10-28 22:22:44 +0900

    20201022

--- readmnist.py
+++ readmnist.py
@@ -1,5 +1,4 @@
 import numpy as np
-import time
 from struct import *
 #파일 읽기
 fp_image = open('t10k-images.idx3-ubyte','rb')
@@ -18,32 +17,25 @@ index=0
 s = fp_image.read(16)    #read first 16byte
 l = fp_label.read(8)     #read first  8byte
 
-k=0 #테스트용 index
-
-
 def divideBy256(x):
     return x / 256
 
 def sixformat(x):
     return format(x, ".6f")
-# while True:
+
 for i in range(101):
     s = fp_image.read(784) #784바이트씩 읽음
     l = fp_label.read(1) #1바이트씩 읽음
-
     if not s:
         break;
     if not l:
         break;
-
     index = int(l[0])
     index = str(index)
     oneimg = unpack(len(s) * 'B', s)
     onearr = oneimg
     newList = list(map(divideBy256, onearr))
     newLists = list(map(sixformat, newList))
-    #파일입출력으로 메모장에 저장
-
     img = np.reshape(unpack(len(s) * 'B', s), (28, 28))
 
     with open('C:/Users/dbstn/Desktop/numbers.txt', 'a') as f:
